e356b7fd65881f4481e149cff82a190e9a72f7ad
Ney Gelbcke Junior <juniorgelbcke@gmail.com>
2023-04-19 16:24:39 -0300

    block_user_after_bad_login_attempts

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -27,6 +27,8 @@ class LoginController extends Controller
      * @var string
      */
     protected $redirectTo = RouteServiceProvider::HOME;
+    protected $maxAttempts  = 3; // Amount of bad attempts user can make
+    protected $decayMinutes = 60; // Time for which user is going to be blocked in minutes
 
     /**
      * Create a new controller instance.
