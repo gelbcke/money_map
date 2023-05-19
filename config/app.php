ee8992c8394ecefc3eeb6903e3334a2f47ed154e
Ney Gelbcke Junior <juniorgelbcke@gmail.com>
2023-05-19 08:11:47 -0300

    update_app_version

--- config/app.php
+++ config/app.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Facade;
 
 return [
 
-    'app_version' => '1.4.5',
+    'app_version' => '1.5.0',
 
     /*
     |--------------------------------------------------------------------------
