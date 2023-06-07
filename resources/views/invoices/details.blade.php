ca8181d5b5c839a7c4a6b815d0b09d14e9cd0788
Ney Gelbcke Junior <juniorgelbcke@gmail.com>
2023-06-07 11:10:28 -0300

    update_invoices_table

--- resources/views/invoices/details.blade.php
+++ resources/views/invoices/details.blade.php
@@ -5,6 +5,13 @@
     'activeNav' => '',
 ])
 
+@section('styles')
+	<!-- DataTables -->
+	<link rel="stylesheet" href="{{ asset('assets') }}/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+	<link rel="stylesheet" href="{{ asset('assets') }}/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
+	<link rel="stylesheet" href="{{ asset('assets') }}/plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
+@endsection
+
 @section('content')
 	<!-- Content Header (Page header) -->
 	<section class="content-header">
@@ -77,7 +84,7 @@
 		<!-- Table row -->
 		<div class="row">
 			<div class="col-12 table-responsive">
-				<table class="table-striped table">
+				<table id="invoice_table" class="table-bordered table-striped table">
 					<thead>
 						<tr>
 							<th style="width: 10%">
@@ -230,6 +237,39 @@
 				viewMode: 'months',
 				format: 'MM-YYYY'
 			})
+		});
+	</script>
+
+	<!-- DataTables  & Plugins -->
+	<script src="{{ asset('assets') }}/plugins/datatables/jquery.dataTables.min.js"></script>
+	<script src="{{ asset('assets') }}/plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
+	<script src="{{ asset('assets') }}/plugins/datatables-responsive/js/dataTables.responsive.min.js"></script>
+	<script src="{{ asset('assets') }}/plugins/datatables-responsive/js/responsive.bootstrap4.min.js"></script>
+	<script src="{{ asset('assets') }}/plugins/datatables-buttons/js/dataTables.buttons.min.js"></script>
+	<script src="{{ asset('assets') }}/plugins/datatables-buttons/js/buttons.bootstrap4.min.js"></script>
+	<script src="{{ asset('assets') }}/plugins/jszip/jszip.min.js"></script>
+	<script src="{{ asset('assets') }}/plugins/pdfmake/pdfmake.min.js"></script>
+	<script src="{{ asset('assets') }}/plugins/pdfmake/vfs_fonts.js"></script>
+	<script src="{{ asset('assets') }}/plugins/datatables-buttons/js/buttons.html5.min.js"></script>
+	<script src="{{ asset('assets') }}/plugins/datatables-buttons/js/buttons.print.min.js"></script>
+	<script src="{{ asset('assets') }}/plugins/datatables-buttons/js/buttons.colVis.min.js"></script>
+
+	<script src="https://cdn.datatables.net/plug-ins/1.10.12/sorting/date-eu.js"></script>
+
+	<script>
+		$(function() {
+			$("#invoice_table").DataTable({
+				"responsive": true,
+				"lengthChange": false,
+				"autoWidth": false,
+				"paging": false,
+				"columnDefs": [{
+					"targets": 0,
+					"type": "date-eu"
+				}],
+				"order": [0, 'desc'],
+				"buttons": ["copy", "csv", "excel", "pdf", "print", "colvis"]
+			}).buttons().container().appendTo('#invoice_table_wrapper .col-md-6:eq(0)');
 		});
 	</script>
 @endsection
